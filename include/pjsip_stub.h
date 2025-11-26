a80ab3b880de4b62a72af75e6777d1767aae86e5
backankurdubey-ops <back.ankurdubey@gmail.com>
2025-11-26 17:52:24 +0530

    Create pjsip_stub.h

---
+++ include/pjsip_stub.h
@@ -0,0 +1,11 @@
+#ifndef PJSIP_STUB_H
+#define PJSIP_STUB_H
+
+#include <string>
+#include <iostream>
+
+bool pjsip_make_call(const std::string& number);
+bool pjsip_send_udptl(const uint8_t* data, size_t len);
+bool pjsip_hangup();
+
+#endif
