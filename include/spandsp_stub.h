d80da4e8a040b2667872f26f776c6f1e65ffc750
backankurdubey-ops <back.ankurdubey@gmail.com>
2025-11-26 17:53:19 +0530

    Create spandsp_stub.h

---
+++ include/spandsp_stub.h
@@ -0,0 +1,11 @@
+#ifndef SPANDSP_STUB_H
+#define SPANDSP_STUB_H
+
+#include <vector>
+#include <iostream>
+
+bool spandsp_start_fax_session();
+bool spandsp_encode_page(const std::vector<uint8_t>& page, std::vector<uint8_t>& outFrames);
+bool spandsp_end_fax_session(std::vector<uint8_t>& finalFrames);
+
+#endif
