5d9fa1eaf8be44c7e7392fdb1b79c6d77f49bc59
Anup <patillanup@gmail.com>
2025-12-05 17:22:26 +0530

    Updating file include/pjsip_stub.h

--- include/pjsip_stub.h
+++ include/pjsip_stub.h
@@ -3,11 +3,11 @@
 
 #include <string>
 #include <iostream>
-#include <cstdint>
-#include <cstddef>
+#include "logger.h"
 
 bool pjsip_make_call(const std::string& number);
 bool pjsip_send_udptl(const uint8_t* data, size_t len);
 bool pjsip_hangup();
+SipMetrics pjsip_get_session_metrics();
 
 #endif
