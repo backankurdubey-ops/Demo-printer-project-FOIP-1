0e773e8889b5bfcac58e32aba57b02e754c02a24
Anup <patillanup@gmail.com>
2025-12-09 11:05:08 +0530

    Updating file include/spandsp_stub.h

--- include/spandsp_stub.h
+++ include/spandsp_stub.h
@@ -3,10 +3,13 @@
 
 #include <vector>
 #include <iostream>
-#include <cstdint>
 
+// Original SpanDSP API
 bool spandsp_start_fax_session();
 bool spandsp_encode_page(const std::vector<uint8_t>& page, std::vector<uint8_t>& outFrames);
 bool spandsp_end_fax_session(std::vector<uint8_t>& finalFrames);
+
+// New metrics hook
+void spandsp_record_frame(size_t frameSize);
 
 #endif
