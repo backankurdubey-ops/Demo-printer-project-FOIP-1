3ff4a6f81eaa4e54d546de7cfbd8e0f9e18f7384
Anup <patillanup@gmail.com>
2025-12-09 11:05:07 +0530

    Updating file include/pjsip_stub.h

--- include/pjsip_stub.h
+++ include/pjsip_stub.h
@@ -4,10 +4,21 @@
 #include <string>
 #include <iostream>
 #include <cstdint>
-#include <cstddef>
 
+// SIP metrics structure
+struct SipMetrics {
+    size_t packetsSent;
+    size_t bytesSent;
+    uint64_t sessionDurationMs;
+};
+
+// Original PJSIP API
 bool pjsip_make_call(const std::string& number);
 bool pjsip_send_udptl(const uint8_t* data, size_t len);
 bool pjsip_hangup();
+
+// New metrics API
+SipMetrics pjsip_get_session_metrics();
+void pjsip_reset_metrics();
 
 #endif
